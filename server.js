a314df3ca5821739f285fbef4f7a2b03ba648a95
Firoz Khan <firozkhan1010@users.noreply.github.com>
2026-09-05 03:56:35 -0700

    fix port number

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ import connectDB from "./database.js";
 
 connectDB();
 
- const PORT = process.env.PORT || 5000;
+ const PORT = process.env.PORT || 8000;
 
- app.listen(PORT, () => {
+ app.listen(PORT,"0,0,0,0", () => {
     console.log(`Server running on port ${PORT}`)
  })
