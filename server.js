3608df5cee6f51710d4304eceb3720de62c28648
Firoz Khan <firozkhan1010@users.noreply.github.com>
2026-09-08 22:02:24 -0700

    first render port binding

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ connectDB();
 
  const PORT = process.env.PORT || 8000;
 
- app.listen(PORT,"0,0,0,0", () => {
+ app.listen(PORT,"0.0.0.0", () => {
     console.log(`Server running on port ${PORT}`)
  })
