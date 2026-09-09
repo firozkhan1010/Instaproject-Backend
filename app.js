d8a87fe0c1ab6be27a1e7ab8f5e8f88f8f2db138
Firoz Khan <firozkhan1010@users.noreply.github.com>
2026-09-08 21:15:35 -0700

    fixed cors configuaration

--- app.js
+++ app.js
@@ -1,34 +1,44 @@
 import express from "express";
 import cors from "cors";
-import User from "./schema.js"
+import User from "./schema.js";
 
 const app = express();
 
 app.use(cors({
-  origin: [
-    "https://instaproject-frontend-kkw6.vercel.app",
-    "https://get-followers-instagram-com.vercel.app",
-    "http://localhost:3000"
-  ],
-  credentials: true
+    origin: [
+        "https://instaproject-frontend-kkw6.vercel.app",
+        "https://get-followers-instagram-com.vercel.app",
+        "http://localhost:3000",
+        "http://localhost:5173"
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true
 }));
 
-app.use (express.json());
+app.use(express.json());
 
-// GET API
-
-app.get ("/", (req, res) => {
-    res.send("home api i running")
+app.get("/", (req, res) => {
+    res.send("home api is running");
 });
-
-//POST API 
 
 app.post("/userdata", async (req, res) => {
-    console.log(req.body);
-    const data = await User.create(req.body)
-    res.json({
-        message: "Data received successfully",
-        data:req.body
-    });
+    try {
+        console.log(req.body);
+
+        const data = await User.create(req.body);
+
+        res.json({
+            message: "Data received successfully",
+            data: req.body
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: "Server error",
+            error: error.message
+        });
+    }
 });
+
 export default app;
